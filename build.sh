d560716dbdc1f120de66950d2c28e27d0ea3d048
HeatRIderHR <kylethompsonyoung@gmail.com>
2026-05-09 22:45:39 +0100

    ImGui testing slider and buttons

---
+++ build.sh
@@ -0,0 +1,6 @@
+mkdir -p build
+
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -B build
+cd build
+make all
+cd ..
--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,18 @@
 #include <imgui.h>
 #include <rlImGui.h>
 
+
 int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "My Game");
-
+	
 	rlImGuiSetup(true);
+	ImGuiIO &io = ImGui::GetIO();
+	io.FontGlobalScale = 2;
 
 	Vector2 poly = {100, 100};
-	float spin = 0;
+	
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
@@ -21,10 +24,24 @@ int main()
 		rlImGuiBegin();
 		ImGui::Begin("test");
 		ImGui::Text("Hi");
-		ImGui::Button("Button");
+		if (ImGui::Button("Button"))
+		{
+			std::cout << "Button" << std::endl;
+		}
+		if (ImGui::Button("Button2"))
+		{
+			std::cout << "Button2" << std::endl;
+		}
 		ImGui::End();
 
-		spin += 0.1;
+		ImGui::Begin("slide");
+		ImGui::Text("hello");
+		ImGui::Separator();
+		ImGui::NewLine();
+		static float spin = 0;
+		ImGui::SliderFloat("slider", &spin, 0, 360);
+		ImGui::End();
+		
 		DrawRectangle(50, 50, 100, 100, {255, 0, 0, 255});
 		DrawPoly(poly, 8, 30, spin, {0, 255, 0, 255});
 
