718e90729199835894fae1eda8ba1a5706459ac0
HeatRIderHR <kylethompsonyoung@gmail.com>
2026-05-09 20:42:03 +0100

    Playing with shapes

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,16 @@ int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "My Game");
-
+	Vector2 poly = {100, 100};
+	float spin = 0;
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-		ClearBackground(RAYWHITE);
+		ClearBackground(BLACK);
 
-		DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
+		spin += 0.1;
+		DrawRectangle(50, 50, 100, 100, {255, 0, 0, 255});
+		DrawPoly(poly, 8, 30, spin, {0, 255, 0, 255});
 
 		EndDrawing();
 	}
