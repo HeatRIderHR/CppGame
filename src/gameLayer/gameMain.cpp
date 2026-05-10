d0b65713df2f658a09deb2fb7a6aafe66e236573
HeatRIderHR <kylethompsonyoung@gmail.com>
2026-05-10 15:45:57 +0100

    Assets and asserts added

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -1,19 +1,30 @@
 #include <raylib.h>
 #include "gameMain.h"
-#include <iostream>
+#include <asserts.h>
+
+struct GameData
+{
+    Texture dirtTexture;
+}gameData;
+
 bool initGame()
 {
-
+    
+    
     return true;
 }
 
 bool updateGame()
 {
-    DrawRectangle(50, 50, 100, 200, {255, 0, 0, 255});
+    float deltaTime = GetFrameTime();
+    if (deltaTime > 1.f / 5) { deltaTime = 1/5.f; }
+
+
+
     return true;
 }
 
 void closeGame()
 {
-    std::cout << "Fin" << std::endl;
+    
 }
